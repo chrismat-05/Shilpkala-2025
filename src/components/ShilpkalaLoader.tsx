70a37b029948dcee0aa29af3a02b0371d6fcc7f4
Chris Mathew Aje <chrismaje63@gmail.com>
2025-09-18 01:00:34 +0530

    fix: update animation to loop infinitely for ShilpkalaLoader component

--- src/components/ShilpkalaLoader.tsx
+++ src/components/ShilpkalaLoader.tsx
@@ -45,7 +45,7 @@ const ShilpkalaLoader: React.FC = () => {
         .animate-stroke {
           stroke-dasharray: 600;
           stroke-dashoffset: 600;
-          animation: outline-draw 1.5s cubic-bezier(0.4,0,0.2,1) forwards;
+          animation: outline-draw 1.5s cubic-bezier(0.4,0,0.2,1) infinite;
         }
         @keyframes outline-draw {
           to {
